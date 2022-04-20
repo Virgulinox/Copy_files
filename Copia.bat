edd5aabd98a54a2cdf4f85c54d071d72dcc1c233
Daniel Marcos de Paula <92738068+Virgulinox@users.noreply.github.com>
2022-04-20 18:16:22 -0300

    Add files via upload

---
+++ Copia.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+SET ORIGEM=C:\Users\Daniel\Desktop\Origem
+SET DESTINO=C:\Users\Daniel\Desktop\DEstino
+
+::LIMPA OS LOGS
+DEL COPIADO.LOG /Q
+DEL FALHA.LOG /Q
+
+for /F "tokens=*" %%A IN (lista.txt) DO CALL :COPIA %%A
+GOTO :EOF
+
+:COPIA
+echo Verificando %1 ...
+IF EXIST %ORIGEM%\%1 (
+	:: se existe copia e grava no log copiado
+	copy %ORIGEM%\%1 %DESTINO% /Y
+
+	echo %1 >> copiado.log
+
+	:: VAI PARA O PROXIMO ARQUIVO
+	GOTO :EOF
+)
+
+:: se não existe apenas grava no log falha
+echo %1 >> falha.log
+	
